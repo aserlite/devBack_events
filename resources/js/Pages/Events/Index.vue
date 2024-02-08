a6f1666256b03e49cea6a87bdcd167934794ea2b
aserlite <arthurcuvillon691@gmail.com>
2024-02-08 15:08:23 +0100

    Pagination

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -1,18 +1,18 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
 
-defineProps({
-    events: Array,
-    storage_path: '',
-})
+const props = defineProps(['events', 'storage_path']);
 
 </script>
 
 <template>
     <AppLayout title="Events">
         <div class="grid grid-cols-3 gap-8">
-            <a v-for="event in events" :key="event.id" :href="route('events.show',event)" class="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-                <img :src="'/storage/' + event.image "  class="h-56 w-full rounded-md object-cover"/>
+            <a v-for="event in events.data" :key="event.id" :href="route('events.show', event)"
+               class="block rounded-lg p-4 shadow-sm shadow-indigo-100">
+                <img :src="'/storage/' + event.image " class="h-56 w-full rounded-md object-cover"/>
 
                 <div class="mt-2">
                     <dl>
@@ -31,7 +31,13 @@ defineProps({
 
                     <div class="mt-6 flex items-center gap-8 text-xs">
                         <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                            <svg style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16"> <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" fill="blue"></path> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" fill="blue"></path> <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" fill="blue"></path> </svg>
+                            <svg style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
+                                <path
+                                    d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"
+                                    fill="blue"></path>
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" fill="blue"></path>
+                                <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" fill="blue"></path>
+                            </svg>
 
                             <div class="mt-1.5 sm:mt-0">
                                 <p class="text-gray-500">Price</p>
@@ -66,5 +72,9 @@ defineProps({
                 </div>
             </a>
         </div>
+
+
+        <Pagination class="mt-6" :links="events.links" />
+
     </AppLayout>
 </template>
